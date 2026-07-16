32af6843d27ac6a48d4ab62c7851266c94ce9e4a
jaflim <118421787+pandesal12@users.noreply.github.com>
2026-07-17 06:16:00 +0800

    Updated Portfolio

--- javascript.js
+++
@@ -1,8 +0,0 @@
-//Change Nav Color:
-
-window.addEventListener("scroll", function() {
-    this.document.querySelector("header").classList.toggle("shiftColor", window.scrollY > 200);
-})
-
-//Persistent Animation on Navigation:
-
